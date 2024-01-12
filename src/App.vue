454d460e0582dfcce25f63780208e24a6bd040c6
wadiaz98 <wadiaz@uce.edu.ec>
2024-01-11 19:47:28 -0500

    prueba

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="puntaje">
+    <p>Puntaje: {{ puntaje }}</p>
+    <p>Intento: {{ intento }}</p>
+  </div>
+  <div class="container">
+    <Prueba
+      ref="uno"
+      img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      texto="xxxxxxxxxxxx"
+    />
+    <Prueba
+      ref="dos"
+      img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg"
+      texto="xxxxxxxxxxxx"
+    />
+    <Prueba
+      ref="tres"
+      img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"
+      texto="xxxxxxxxxxxx"
+    />
+    <div class="boton">
+      <button v-on:click="juega(), comprueba(), intentos()">JUGAR</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Prueba from "./components/prueba.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Prueba,
+  },
+  data() {
+    return {
+      puntaje: 0,
+      intento: 0,
+      puntos: null,
+    };
+  },
+  methods: {
+    juega() {
+      this.$refs.uno.consumirApi();
+      this.$refs.dos.consumirApi();
+      this.$refs.tres.consumirApi();
+    },
+
+    comprueba() {
+      const respuestas = [
+        this.$refs.uno.texto,
+        this.$refs.dos.texto,
+        this.$refs.tres.texto,
+      ];
+      const puntos = this.puntos;
+      for ( let i = 0; i < respuestas.length; i++) {
+        if (respuestas[i] === "yes") {
+          puntos++;
+        }
+      }
+      if (puntos === 3) {
+        this.puntaje += 5;
+      } else if (puntos === 2) {
+        rgis.puntaje += 2;
+      } else if (puntos === 1) {
+        this.puntaje += 1;
+      } else if (puntos === 0) {
+        this.puntaje += 0;
+      }
+    },
+    intentos() {
+      this.intento++;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +84,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+}
+.puntaje {
+  display: grid;
+  grid-template-columns: repeat(2, 500px);
+  gap: 1px;
+}
+.boton {
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  grid-column: 2;
 }
 </style>
---
+++ src/components/prueba.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    
+    <div class="imagenes">
+      <img v-bind:src="img" />
+      <p>{{texto}}</p>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["img", "texto"],
+  data() {
+    return {
+      img: this.img,
+      texto: this.texto,
+    };
+  },
+  methods: {
+    async consumirApi() {
+      const { answer, image } = await fetch("https://yesno.wtf/api").then(
+        (respuesta) => respuesta.json()
+        
+      );
+      this.img = image;
+      this.texto=answer;
+    },
+    comprueba(){
+        
+    }
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  grid-template-columns: repeat(3, 330px);
+}
+img {
+  width: 200px;
+  height: 200px;
+}
+
+</style>
